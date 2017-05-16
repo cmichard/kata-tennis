0fe5a4eac2632bfd300b826ae8321cc501268c2b
Christian MICHARD <christian.michard@softeam.fr>
2017-05-16 08:52:49 +0200

    sprint1 user story1 tests

---
+++ KataTennis/TennisTests/PlayerGameScoreTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisTests
+{
+    [TestClass]
+    public class PlayerGameScoreTest
+    {
+        [TestMethod]
+        public void ShouldStartAtZero()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldGetFifteen()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldGetThirty()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldGetFourty()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ShouldWin()
+        {
+            Assert.Fail();
+        }
+    }
+}
