2707ceaeafdfa28bcb3ba0e4f9b0e8b8f20b70c2
Christian MICHARD <christian.michard@softeam.fr>
2017-05-16 09:39:06 +0200

    deuce rule tests

---
+++ KataTennis/TennisScore/GameScore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisScore
+{
+    /// <summary>
+    /// Tennis game score.
+    /// </summary>
+    public class GameScore
+    {
+        public string Score { get; private set; }
+
+        /// <summary>
+        /// First player game score.
+        /// </summary>
+        public PlayerGameScore RafaGameScore { get; private set; }
+
+        /// <summary>
+        /// Second player game score.
+        /// </summary>
+        public PlayerGameScore JokoGameScore { get; private set; }
+
+        /// <summary>
+        /// Player 1 wins point.
+        /// </summary>
+        public void RafaWinPoint()
+        {
+
+        }
+
+        /// <summary>
+        /// Player 2 wins point.
+        /// </summary>
+        public void JokoWinPoint()
+        {
+
+        }
+    }
+}
---
+++ KataTennis/TennisTests/GameScoreTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisScore;
+
+namespace TennisTests
+{
+    [TestClass]
+    public class GameScoreTest
+    {
+        [TestMethod]
+        public void RafaShouldWin()
+        {
+            var score = new GameScore();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            Assert.AreEqual("30", score.JokoGameScore.Points);
+            Assert.AreEqual("game", score.RafaGameScore.Points);
+            Assert.AreEqual("game rafa", score.Score);
+        }
+
+        [TestMethod]
+        public void ShouldDeuce()
+        {
+            var score = new GameScore();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            Assert.AreEqual("40", score.JokoGameScore.Points);
+            Assert.AreEqual("40", score.RafaGameScore.Points);
+            Assert.AreEqual("deuce", score.Score);
+        }
+
+        [TestMethod]
+        public void DeuceShouldAdvantage()
+        {
+            var score = new GameScore();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            Assert.AreEqual("advantage rafa", score.RafaGameScore.Points);
+        }
+
+        [TestMethod]
+        public void AdvantageShouldWinGame()
+        {
+            var score = new GameScore();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            Assert.AreEqual("game rafa", score.RafaGameScore.Points);
+        }
+
+        [TestMethod]
+        public void AdvantageShouldBackToDeuce()
+        {
+            var score = new GameScore();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.JokoWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.RafaWinPoint();
+            score.JokoWinPoint();
+            Assert.AreEqual("deuce", score.RafaGameScore.Points);
+        }
+    }
+}
